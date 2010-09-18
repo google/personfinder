c48fec4ec716f8bc186baf6b274b965f3ee9520b
Ka-Ping Yee <ping@zesty.ca>
2010-09-17 18:55:23 -0700

    Don't wipe Counters when wiping the datastore.

--- tools/setup.py
+++ tools/setup.py
@@ -25,7 +25,7 @@ def setup_datastore():
 def wipe_datastore(*kinds):
     """Deletes everything in the datastore except Accounts and Secrets.
     If 'kinds' is given, deletes only those kinds of entities."""
-    for kind in kinds or [Person, Note, Photo, Authorization, Counter,
+    for kind in kinds or [Person, Note, Photo, Authorization,
                           Subdomain, config.ConfigEntry]:
         keys = kind.all(keys_only=True).fetch(200)
         while keys:
