af397916201f4710ae35bb3e73550f5786a9619b
Lee Schumacher <lschumacher@google.com>
2011-02-14 18:23:36 -0800

    Quick shell script for running in common.sh env (needs no running dev server unlike console)

---
+++ tests/shell.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python2.5
+# Copyright 2010 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Runs the unit tests, with stubs for the datastore API.
+
+Instead of running this script directly, use the 'unit_tests' shell script,
+which sets up the PYTHONPATH and other necessary environment variables."""
+
+import os
+import sys
+import unittest
+
+from google.appengine.api import apiproxy_stub_map
+from google.appengine.api import datastore_file_stub
+from google.appengine.ext import db
+
+import remote_api
+
+# Create a new apiproxy and temp datastore to use for this test suite
+apiproxy_stub_map.apiproxy = apiproxy_stub_map.APIProxyStubMap()
+temp_db = datastore_file_stub.DatastoreFileStub(
+    'PersonFinderUnittestDataStore', None, None, trusted=True)
+apiproxy_stub_map.apiproxy.RegisterStub('datastore', temp_db)
+
+# An application id is required to access the datastore, so let's create one
+os.environ['APPLICATION_ID'] = 'personfinder-unittest'
+
