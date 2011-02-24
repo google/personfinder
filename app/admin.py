562de1c1ae8d8bbaed41c87c14506a7179f5b77f
Raquel Romano <romano@google.com>
2011-02-24 15:22:25 -0800

    Responded to Ping's comments.

--- app/admin.py
+++ app/admin.py
@@ -66,8 +66,8 @@ class Admin(Handler):
                 search_auth_key_required=True,
                 deactivated=False,
                 deactivation_message_html='',
-                main_page_footer_html='',
-                results_page_footer_html='',
+                main_page_custom_html='',
+                results_page_custom_html='',
             )
             self.redirect('/admin', subdomain=self.params.subdomain_new)
 
@@ -88,7 +88,7 @@ class Admin(Handler):
                         400, 'The setting for %s was not valid JSON.' % name)
 
             for name in ['keywords', 'deactivation_message_html',
-                         'main_page_footer_html', 'results_page_footer_html']:
+                         'main_page_custom_html', 'results_page_custom_html']:
                 # These settings are literal strings (not JSON).
                 values[name] = self.request.get(name)
 
