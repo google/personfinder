a7257e4b1a4e8d5ddfa5fd759bfe2078d0234a10
Lee Schumacher <lschumacher@google.com>
2011-03-13 14:48:07 +1100

    Adding in max_results for search api.

--- app/api.py
+++ app/api.py
@@ -128,6 +128,8 @@ class Search(utils.Handler):
         # Retrieve parameters and do some sanity checks on them.
         query_string = self.request.get("q")
         subdomain = self.request.get("subdomain")        
+        max_results = self.params.max_results or 100
+
         if not query_string:
             return self.error(400, 'Missing q parameter')
         if not subdomain:
@@ -135,7 +137,7 @@ class Search(utils.Handler):
    
         # Perform the search.
         results = indexing.search(Person.all_in_subdomain(subdomain),
-                                  TextQuery(query_string), 100)
+                                  TextQuery(query_string), max_results)
 
         records = [pfif_version.person_to_dict(result) for result in results]
         utils.optionally_filter_sensitive_fields(records, self.auth)
