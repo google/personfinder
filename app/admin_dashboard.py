d50b1d98d932b4384827b6277e919137b402b0fd
Raquel Romano <romano@google.com>
2011-03-17 22:20:48 -0700

    Responded to comments from kpy.

--- app/admin_dashboard.py
+++ app/admin_dashboard.py
@@ -86,23 +86,15 @@ class Dashboard(Handler):
                 (name, Counter.get_count(subdomain, name))
                 for name in counter_names)
 
-        data['person_original_domains'] = {}
-        for subdomain in subdomains:
-            counts = Counter.get_all_counts(subdomain, 'person')
-            domain_count_pairs = [
-                (name.split('=', 1)[1], counts[name])
-                for name in counts if name.startswith('original_domain=')]
-            data['person_original_domains'][subdomain] = sorted(
-                domain_count_pairs, key=lambda pair: -pair[1])
-
-        data['note_original_domains'] = {}
-        for subdomain in subdomains:
-            counts = Counter.get_all_counts(subdomain, 'note')
-            domain_count_pairs = [
-                (name.split('=', 1)[1], counts[name])
-                for name in counts if name.startswith('original_domain=')]
-            data['note_original_domains'][subdomain] = sorted(
-                domain_count_pairs, key=lambda pair: -pair[1])
+        for kind in ['person', 'note']:
+            data[kind + '_original_domains'] = {}
+            for subdomain in subdomains:
+                counts = Counter.get_all_counts(subdomain, kind)
+                domain_count_pairs = [
+                    (name.split('=', 1)[1], counts[name])
+                    for name in counts if name.startswith('original_domain=')]
+                data[kind + '_original_domains'][subdomain] = sorted(
+                    domain_count_pairs, key=lambda pair: -pair[1])
 
         # Encode the data as JSON.
         json = simplejson.dumps(data, default=encode_date)
