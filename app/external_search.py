666cbadd50f7e96ee1c4632d58883dae37137fc8
Ryo Kawaguchi <ryok@google.com>
2011-03-31 15:06:25 +0900

    Bug fix: missing subdomain in the key names.  Add more logging.

--- app/external_search.py
+++ app/external_search.py
@@ -93,6 +93,8 @@ def search(subdomain, query_obj, max_results, backends):
     except:
         logging.warn('Fetched content is broken.')
         return None
+    logging.debug('external_search.search fetched name: %d, all: %d' %
+                  (len(data['name_entries']), len(data['all_entries'])))
 
     # The entries returned from backends may include ones that are already taken
     # down in the production repository.  We need to ensure those are not
@@ -102,16 +104,21 @@ def search(subdomain, query_obj, max_results, backends):
         return []
     # TODO(ryok): Remove once the backends stop returning the old data format.
     if isinstance(ids[0], dict):
-        ids = ['%s:%s' % (subdomain, d['person_record_id']) for d in ids]
-    persons = model.Person.get_by_key_name(ids)
+        ids = [d['person_record_id'] for d in ids]
+    key_names = ['%s:%s' % (subdomain, id) for id in ids]
+    persons = model.Person.get_by_key_name(key_names)
     address_match_begin = len(data['name_entries'])
     name_matches = [p for p in persons[:address_match_begin] if p]
     address_matches = [p for p in persons[address_match_begin:] if p]
+    logging.debug('external_search.search matches name: %d, all: %d' %
+                  (len(name_matches), len(address_matches)))
 
     name_matches.sort(indexing.CmpResults(query_obj))
     all_matches = name_matches
     if address_matches:
         address_matches = remove_non_name_matches(address_matches, query_obj)
+        logging.debug('address_matches after remove_non_name_matches: %d' %
+                      len(address_matches))
         address_matches.sort(indexing.CmpResults(query_obj))
         address_matches[0].address_match_begins = True
         all_matches += address_matches
