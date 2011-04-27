c9cbd3474a639dca77b99c229024ae37eaff238d
Lee Schumacher <lschumacher@google.com>
2011-04-27 12:25:25 -0700

    Handle bad subdomain in main page.

--- app/main.py
+++ app/main.py
@@ -24,15 +24,15 @@ class Main(Handler):
         redirect_url = self.maybe_redirect_jp_tier2_mobile()
         if redirect_url:
             return self.redirect(redirect_url)
-
-        if not self.subdomain:
+        subdomains = Subdomain.list()
+        if not self.subdomain or not self.subdomain in subdomains:
             self.write('''
 <style>body { font-family: arial; font-size: 13px; }</style>
 <p>Select a Person Finder site:<ul>
 ''')
-            for key in Subdomain.all(keys_only=True):
-                url = self.get_start_url(key.name())
-                self.write('<li><a href="%s">%s</a>' % (url, key.name()))
+            for sd in subdomains:
+                url = self.get_start_url(sd)
+                self.write('<li><a href="%s">%s</a>' % (url, sd))
             self.write('</ul>')
             return
 
