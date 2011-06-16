4b43f05f5f6d608a03fe17734f280763a252dbff
Sanath Kumar Ramesh <sanathkumar@google.com>
2011-06-16 15:19:36 -0700

    Changed copyright year

--- app/admin.py
+++ app/admin.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# this is a test comment
 
 from datetime import datetime
 import simplejson
