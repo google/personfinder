5d1e75a6f96ee9f43277cacaa39dd06606280d77
Ka-Ping Yee <kpy@google.com>
2012-01-17 23:50:01 -0800

    Clean up unused settings in setup_pf.py.

--- tools/setup_pf.py
+++ tools/setup_pf.py
@@ -44,13 +44,12 @@ def reset_datastore():
 def setup_repos():
     db.put([Repo(key_name='haiti'),
             Repo(key_name='japan'),
-            Repo(key_name='pakistan'),
-            Repo(key_name='lang-test')])
+            Repo(key_name='pakistan')])
     # Set some repositories active so they show on the main page.
-    config.set(active_repos=['japan', 'haiti', 'lang-test'])
+    config.set(active_repos=['japan', 'haiti'])
 
 def setup_configs():
-    """Installs the configuration settings for Haiti, Chile, China, Pakistan."""
+    """Installs configuration settings used for testing by server_tests."""
     COMMON_KEYWORDS = ['person', 'people', 'finder', 'person finder',
                        'people finder', 'crisis', 'survivor', 'family']
 
