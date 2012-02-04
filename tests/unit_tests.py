c40bdd5be3509fd8a886250e1d53be966f5f2594
Ka-Ping Yee <kpy@google.com>
2012-02-04 20:52:03 +1100

    Improve a comment.

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -26,17 +26,17 @@ from google.appengine.api import datastore_file_stub
 
 # Create a new apiproxy and temp datastore to use for this test suite
 apiproxy_stub_map.apiproxy = apiproxy_stub_map.APIProxyStubMap()
-temp_db = datastore_file_stub.DatastoreFileStub(
-    'PersonFinderUnittestDataStore', None, None, trusted=True)
+temp_db = datastore_file_stub.DatastoreFileStub('x', None, None, trusted=True)
 apiproxy_stub_map.apiproxy.RegisterStub('datastore', temp_db)
 
 # An application id is required to access the datastore, so let's create one
 os.environ['APPLICATION_ID'] = 'personfinder-unittest'
 
-# When the appserver is running, the APP_DIR is the current directory...
+# When the appserver is running, the APP_DIR should be the current directory...
 os.chdir(os.environ['APP_DIR'])
 
 # ...but we want pytest to default to finding tests in TESTS_DIR, not the cwd.
+# So, when no arguments are given, we make the option parser return TESTS_DIR.
 import _pytest.config
 original_parse_setoption = _pytest.config.Parser.parse_setoption
 _pytest.config.Parser.parse_setoption = \
