84974ce3b866f8482d04a3691ba8e36f6c9dadbb
Hiroshi Ichikawa <ichikawa@google.com>
2012-03-28 17:19:17 +0900

    Applying review comments.

--- tools/delete_old_entries.py
+++ tools/delete_old_entries.py
@@ -17,20 +17,18 @@
 
 Example command to preview expired/deleted entries:
   $ ./tools/delete_old_entries.sh \
-    --host=mypersonfinder.appspot.com:80 \
+    mypersonfinder.appspot.com \
     --repo=myrepo \
     --email=example@example.com \
-    --password_path=password.txt \
-    --expire_seconds=3600 \
+    --min_age_seconds=3600 \
     --action=preview
 
 Example command to actually expire entries:
   $ ./tools/delete_old_entries.sh \
-    --host=mypersonfinder.appspot.com:80 \
+    mypersonfinder.appspot.com \
     --repo=myrepo \
     --email=example@example.com \
-    --password_path=password.txt \
-    --expire_seconds=3600 \
+    --min_age_seconds=3600 \
     --action=expire
 
 NOTE: Execute delete_old_entries.sh instead of executing delete_old_entries.py
@@ -52,10 +50,15 @@ import remote_api
 import utils
 
 
-def expire_or_delete_person(person, action):
-    """Expires or deletes a person record and associated data."""
+# Max number of entities in one fetch. Fetch fails if we fetch too many
+# entities at once.
+MAX_ENTITIES_PER_REQUEST = 100
+
+
+def expire_person(person):
+    """Expires a person record and associated data."""
     person_text = person_to_text(person)
-    if action == 'expire' and person.is_original():
+    if person.is_original():
         # Set the expiry_date to now, and set is_expired flags to match.
         # (The externally visible result will be as if we overwrote the
         # record with an expiry date and blank fields.)
@@ -73,18 +76,18 @@ def expire_or_delete_person(person, action):
 
 def person_to_text(person):
     """Returns the person's information as string."""
-    return ('id=%s first_name=%s last_name=%s last_modified=%s' % (
+    return ('id=%s first_name=%s last_name=%s entry_date=%s' % (
         person.get_record_id(),
         person.first_name,
         person.last_name,
-        person.last_modified.isoformat(),
+        person.entry_date.isoformat(),
     ))
 
 
 def main():
     logging.basicConfig(file=sys.stderr, level=logging.INFO)
 
-    parser = optparse.OptionParser()
+    parser = optparse.OptionParser(usage='%prog [options] <appserver_url>')
     parser.add_option('--host',
                       help='Host name. e.g. mypersonfinder.appspot.com:80')
     parser.add_option('--repo',
@@ -94,84 +97,91 @@ def main():
                             'The user must be admin of the application.'))
     parser.add_option('--password_path',
                       help=('A text file which contains the password of '
-                            'the user specified by --email.'))
+                            'the user specified by --email. If omitted, '
+                            'the command prompts you to input password.'))
     parser.add_option('--action',
                       type='choice',
-                      choices=['expire', 'delete', 'preview'],
+                      choices=['expire', 'preview'],
                       help=('Either of:\n'
                             'expire: Marks old entries as expired. They '
-                            'disappear from front-end, but are kept in the '
-                            'data store. They are automatically deleted '
-                            'later. Note that clone records are deleted '
-                            'immediately.\n'
-                            'delete: Deletes old entries completely from the '
-                            'data store immediately.\n'
+                            'disappear from the UI and the API, but are kept '
+                            'in the data store. They are automatically '
+                            'deleted later. Note that clone records are '
+                            'deleted immediately.\n'
                             'preview: Does nothing, but dumps entries which '
-                            'will be deleted.'))
-    parser.add_option('--expire_seconds',
+                            'will be marked as expired.'))
+    parser.add_option('--min_age_seconds',
                       type='int',
                       default=3600,
-                      help=('Deletes/expires entries whose last modification '
-                            'time is older than this value in seconds.'))
+                      help=('Expires entries whose entry_date '
+                            'is older than this value in seconds.'))
     parser.add_option('--id_whitelist',
                       help=('Comma-separated person IDs which are never '
                             'deleted. e.g. '
                             'example.personfinder.google.org/person.1234,'
                             'example.personfinder.google.org/person.5678'))
-    parser.add_option('--include_expired',
-                      default='false',
-                      help='Includes expired entries as target.')
     parser.add_option('--dump_all',
                       default='false',
                       help='Also dumps entries which are not deleted.')
+    parser.add_option('--include_expired',
+                      default='false',
+                      help='Includes expired entries on --dump_all.')
     options, args = parser.parse_args()
 
-    if not options.host:
-        logging.fatal('--host is missing.')
-        sys.exit()
+    if len(args) != 1:
+        parser.error('Just one argument must be given.')
+    host = args[0]
     if not options.repo:
-        logging.fatal('--repo is missing.')
-        sys.exit()
+        parser.error('--repo is missing.')
     if not options.email:
-        logging.fatal('--email is missing.')
-        sys.exit()
-    if not options.password_path:
-        logging.fatal('--password_path is missing.')
-        sys.exit()
+        parser.error('--email is missing.')
     if not options.action:
-        logging.fatal('--action is missing.')
-        sys.exit()
+        parser.error('--action is missing.')
 
-    f = open(options.password_path)
-    password = f.read().rstrip('\n')
-    f.close()
+    if options.password_path:
+      f = open(options.password_path)
+      password = f.read().rstrip('\n')
+      f.close()
+    else:
+      password = None
+
     if options.id_whitelist:
         id_whitelist = options.id_whitelist.split(',')
     else:
         id_whitelist = []
 
-    remote_api.connect(options.host,
-                       options.email, password)
+    remote_api.connect(host, options.email, password)
 
-    deleted_entries = []
-    expire_time = (
+    expired_entries = []
+    max_entry_date = (
             datetime.datetime.utcnow() -
-            datetime.timedelta(seconds=options.expire_seconds))
-    for person in model.Person.all_in_repo(
-            options.repo, filter_expired=options.include_expired != 'true'):
-        if (person.last_modified < expire_time and
-                (not person.get_record_id() in id_whitelist)):
-            logging.info('To be expired/deleted: %s' % person_to_text(person))
-            # Appends the person to the list and deletes it later.
-            # Deleting during iteration in the data store can cause unexpedted
-            # result.
-            deleted_entries.append(person)
-        elif options.dump_all == 'true':
-            logging.info('To be kept: %s' % person_to_text(person))
+            datetime.timedelta(seconds=options.min_age_seconds))
+    query = model.Person.all_in_repo(
+            options.repo, filter_expired=options.include_expired != 'true')
+    if options.dump_all != 'true':
+        query.filter('entry_date <=', max_entry_date)
 
-    if options.action in ('expire', 'delete'):
-        for person in deleted_entries:
-            expire_or_delete_person(person, options.action)
+    cursor = None
+    while True:
+        if cursor:
+            people = query.with_cursor(cursor).fetch(MAX_ENTITIES_PER_REQUEST)
+        else:
+            people = query.fetch(MAX_ENTITIES_PER_REQUEST)
+        if not people: break
+        for person in people:
+            # Checks entry_date again because the filter is not applied when
+            # --dump_all=true.
+            if (person.entry_date <= max_entry_date and
+                    person.get_record_id() not in id_whitelist):
+                logging.info('To be expired: %s' % person_to_text(person))
+                expired_entries.append(person)
+            elif options.dump_all == 'true':
+                logging.info('To be kept: %s' % person_to_text(person))
+        cursor = query.cursor()
+
+    if options.action == 'expire':
+        for person in expired_entries:
+            expire_person(person)
 
 
 if __name__ == '__main__':
