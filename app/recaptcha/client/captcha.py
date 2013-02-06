a1673dcbd1aa8db921febb2dedfd767042b55f15
Ka-Ping Yee <kpy@google.com>
2013-02-05 16:04:51 -0800

    Merge three small changes from release-34:   - captcha.py now points to the updated servers at www.google.com/recaptcha   - base.html.template adds the message "To delete your data, access Person Finder from your desktop."   - results.html.template removes the obsolete warning "Note: Alternate names may be machine generated and may not be accurate."

--- app/recaptcha/client/captcha.py
+++ app/recaptcha/client/captcha.py
@@ -6,8 +6,10 @@ import urllib2
 
 import simplejson
 
-API_SSL_SERVER = 'https://api-secure.recaptcha.net'
-API_SERVER = 'http://api.recaptcha.net'
+API_SSL_SERVER = 'https://www.google.com/recaptcha/api'
+# We leave out the URL scheme so that browsers won't complain when accessed
+# from a secure website even when use_ssl is not set.
+API_SERVER = '//www.google.com/recaptcha/api'
 VERIFY_SERVER = 'api-verify.recaptcha.net'
 
 class RecaptchaResponse(object):
