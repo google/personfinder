f8c79834e3f91a3378d82a7984f83bf8f19cb7d7
Ryo Kawaguchi <ryok@google.com>
2013-04-26 01:27:46 +0900

    Add data source filter to the review console.

--- app/admin_review.py
+++ app/admin_review.py
@@ -18,6 +18,7 @@ import logging
 from google.appengine.ext import db
 from google.appengine.api import users
 
+import const
 import model
 import utils
 
@@ -38,27 +39,54 @@ class Handler(utils.BaseHandler):
         if not self.is_current_user_authorized():
             return self.redirect(users.create_login_url('/admin/review'))
 
+        #
         # Make the navigation links.
         status = self.request.get('status') or 'all'
-        nav_html = ''
+        source = self.request.get('source') or 'all'
+
+        status_nav_html = ''
         for option in [
             'all', 'unspecified', 'information_sought', 'is_note_author',
             'believed_alive', 'believed_missing', 'believed_dead']:
             if option == status:
-                nav_html += '<b>%s</b>&nbsp; ' % option
+                status_nav_html += '<b>%s</b>&nbsp; ' % option
             else:
-                nav_html += '<a href="%s">%s</a>&nbsp; ' % (
-                    self.get_url('/admin/review', status=option), option)
+                status_nav_html += '<a href="%s">%s</a>&nbsp; ' % (
+                    self.get_url('/admin/review', status=option, source=source),
+                    option)
 
+        source_nav_html = ''
+        source_options = ['all', '%s.%s' % (self.repo, const.HOME_DOMAIN)]
+        for auth_key in model.Authorization.all().filter('repo =', self.repo):
+            if auth_key.domain_write_permission:
+                source_options.append(auth_key.domain_write_permission)
+        for option in source_options:
+            if option == source:
+                source_nav_html += '<b>%s</b>&nbsp; ' % option
+            else:
+                source_nav_html += '<a href="%s">%s</a>&nbsp; ' % (
+                    self.get_url('/admin/review', status=status, source=option),
+                    option)
+
+        #
         # Construct the query for notes.
         query = model.Note.all_in_repo(self.repo
                          ).filter('reviewed =', False
-                         ).filter('hidden =', False
-                         ).order('-entry_date')
+                         ).filter('hidden =', False)
         if status == 'unspecified':
             query.filter('status =', '')
         elif status != 'all':
             query.filter('status =', status)
+        if source != 'all':
+            query.filter('person_record_id >=', '%s/' % source)
+            query.filter('person_record_id <', '%s0' % source)
+            # TODO(ryok): we really want to order by entry_date, but GAE
+            # restriction applies here, and we can not use two different
+            # properties for comparison and ordering. The proper solution seems
+            # to add a property source_domain to Note.
+            query.order('-person_record_id')
+        else:
+            query.order('-entry_date')
 
         skip = self.params.skip or 0
         notes = query.fetch(NOTES_PER_PAGE + 1, skip)
@@ -82,14 +110,17 @@ class Handler(utils.BaseHandler):
             notes = notes[:NOTES_PER_PAGE]
             next_skip = skip + NOTES_PER_PAGE
             next_url = self.get_url(
-                '/admin/review', skip=str(next_skip), status=status)
+                '/admin/review', skip=str(next_skip),
+                status=status, source=source)
         else:
             next_url = None
 
         return self.render(
             'admin_review.html',
             notes=notes,
-            nav_html=nav_html, next_url=next_url,
+            status_nav_html=status_nav_html,
+            source_nav_html=source_nav_html,
+            next_url=next_url,
             first=skip + 1,
             last=skip + len(notes[:NOTES_PER_PAGE]))
 
@@ -108,7 +139,9 @@ class Handler(utils.BaseHandler):
                         note.hidden = True
                     notes.append(note)
         db.put(notes)
-        self.redirect('/admin/review', status=self.params.status)
+        self.redirect('/admin/review',
+                      status=self.params.status,
+                      source=self.params.source)
 
     def is_current_user_authorized(self):
         if users.is_current_user_admin():  # admins can always review
