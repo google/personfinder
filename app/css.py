4a6eb36cb95c7d6b3b647fb511dec89bc54a245f
kevinalle <kevinalle@gmail.com>
2015-04-28 16:53:25 +0900

    Revert "Redesign the UI"

--- app/css.py
+++ app/css.py
@@ -23,7 +23,7 @@ class Handler(BaseHandler):
 
     def get(self):
         self.response.headers['Content-Type'] = 'text/css'
-        if self.env.ui in ['small', 'light', 'new']:
+        if self.env.ui in ['small', 'light']:
             template_name = 'css-%s' % self.env.ui
         else:
             template_name = 'css-default'
