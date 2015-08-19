eaa4e00248d9d129b064667d48f54ce73be82428
Carlo Lobrano <c.lobrano@gmail.com>
2015-08-20 00:39:03 +0200

    Fix host's personfinder folder path

--- docker/run-container.sh
+++ docker/run-container.sh
@@ -3,4 +3,4 @@
 IMAGE_NAME=$1
 CONTAINER_NAME=$2
 
-docker run --name ${CONTAINER_NAME} -v ~/workspace/personfinder:/opt/personfinder -p 127.0.0.1:8000:8000 -it ${IMAGE_NAME} /bin/bash
+docker run --name ${CONTAINER_NAME} -v ../:/opt/personfinder -p 127.0.0.1:8000:8000 -it ${IMAGE_NAME} /bin/bash
