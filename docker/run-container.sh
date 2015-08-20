422098b71e18eff131eba7cb77e4880cb353df24
Carlo Lobrano <Carlo.Lobrano@telit.com>
2015-08-20 15:31:39 +0200

    Fix run-container.sh folder sharing

--- docker/run-container.sh
+++ docker/run-container.sh
@@ -3,4 +3,11 @@
 IMAGE_NAME=$1
 CONTAINER_NAME=$2
 
-docker run --name ${CONTAINER_NAME} -v ../:/opt/personfinder -p 127.0.0.1:8000:8000 -it ${IMAGE_NAME} /bin/bash
+PRJ_DIR=$(dirname $(pwd))
+
+if [ ! -z ${CONTAINER_NAME} ]; then
+    docker run --name ${CONTAINER_NAME} -v ${PRJ_DIR}:/opt/personfinder -p 127.0.0.1:8000:8000 -it ${IMAGE_NAME} /bin/bash
+else
+    docker run -v ${PRJ_DIR}:/opt/personfinder -p 127.0.0.1:8000:8000 -it ${IMAGE_NAME} /bin/bash
+fi
+
