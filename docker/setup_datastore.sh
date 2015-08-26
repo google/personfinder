3cf548861a0b284349238076b79268d943e5d1b5
Carlo Lobrano <c.lobrano@gmail.com>
2015-08-27 00:25:17 +0200

    Remove delay in setup_datastore.sh script

--- docker/setup_datastore.sh
+++ docker/setup_datastore.sh
@@ -3,11 +3,9 @@
 
 PORT=8000
 IP_ADDR=`ifconfig eth0 | grep -o "addr:[0-9][0-9][0-9]\.[0-9]*\.[0-9]*\.[0-9]*" | awk -F ":" '{print $2}'`
-SLEEP=10
 
 if [ 0 = ${INIT_DATASTORE} ]; then
-    echo "Setting datastore for server at ${IP_ADDR}:${PORT} in ${SLEEP} seconds"
-    sleep ${SLEEP}
+    echo "Setting datastore for server at ${IP_ADDR}:${PORT}"
     cd ${PERSONFINDER_DIR}
     tools/console ${IP_ADDR}:${PORT} -c 'setup_datastore()' && export INIT_DATASTORE=1 || echo "Setting datastore failed!"
 fi
