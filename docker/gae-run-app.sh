d217b0757cf0a5ecdf3fdebe4c727a9d81a2de8d
Carlo Lobrano <c.lobrano@gmail.com>
2015-08-26 23:21:04 +0200

    Update gae-run-app script and added a script to update the datastore

--- docker/gae-run-app.sh
+++ docker/gae-run-app.sh
@@ -4,4 +4,6 @@ PORT=8000
 IP_ADDR=`ifconfig eth0 | grep -o "addr:[0-9][0-9][0-9]\.[0-9]*\.[0-9]*\.[0-9]*" | awk -F ":" '{print $2}'`
 
 cd ${PERSONFINDER_DIR}
+echo "Starting Person Finder server at ${IP_ADDR}:${PORT}"
 tools/gae run app --port=${PORT} --host=${IP_ADDR}
+echo "Person Finder server stopped"
---
+++ docker/setup_datastore.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Initialize the Person Finder's datastore the first time it is run on this machine.
+
+PORT=8000
+IP_ADDR=`ifconfig eth0 | grep -o "addr:[0-9][0-9][0-9]\.[0-9]*\.[0-9]*\.[0-9]*" | awk -F ":" '{print $2}'`
+SLEEP=10
+
+if [ 0 = ${INIT_DATASTORE} ]; then
+    echo "Setting datastore for server at ${IP_ADDR}:${PORT} in ${SLEEP} seconds"
+    sleep ${SLEEP}
+    cd ${PERSONFINDER_DIR}
+    tools/console ${IP_ADDR}:${PORT} -c 'setup_datastore()' && export INIT_DATASTORE=1 || echo "Setting datastore failed!"
+fi
