c070136356919fd6b0cbbd93a557e57c2d4b3ba7
Carlo Lobrano <c.lobrano@gmail.com>
2015-08-27 00:18:34 +0200

    Set admin host's IP address

--- docker/gae-run-app.sh
+++ docker/gae-run-app.sh
@@ -5,5 +5,5 @@ IP_ADDR=`ifconfig eth0 | grep -o "addr:[0-9][0-9][0-9]\.[0-9]*\.[0-9]*\.[0-9]*" 
 
 cd ${PERSONFINDER_DIR}
 echo "Starting Person Finder server at ${IP_ADDR}:${PORT}"
-tools/gae run app --port=${PORT} --host=${IP_ADDR}
+tools/gae run app --port ${PORT} --host ${IP_ADDR} --admin_host=${IP_ADDR}
 echo "Person Finder server stopped"
