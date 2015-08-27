9070b383c58cef4f5f5ad0f7722f15238c6c7dde
Carlo Lobrano <c.lobrano@gmail.com>
2015-08-27 22:36:36 +0200

    Remove unnecessary arguments

--- docker/run-container.sh
+++ docker/run-container.sh
@@ -6,8 +6,8 @@ CONTAINER_NAME=$2
 PRJ_DIR=$(dirname $(pwd))
 
 if [ ! -z ${CONTAINER_NAME} ]; then
-    docker run --name ${CONTAINER_NAME} -v ${PRJ_DIR}:/opt/personfinder -p 127.0.0.1:8000:8000 -it ${IMAGE_NAME} /bin/bash
+    docker run --name ${CONTAINER_NAME} -v ${PRJ_DIR}:/opt/personfinder -it ${IMAGE_NAME} /bin/bash
 else
-    docker run -v ${PRJ_DIR}:/opt/personfinder -p 127.0.0.1:8000:8000 -it ${IMAGE_NAME} /bin/bash
+    docker run -v ${PRJ_DIR}:/opt/personfinder -it ${IMAGE_NAME} /bin/bash
 fi
 
