fced2f58a099a5f4163c3ca8337912af55682db9
ezbr <ezb@google.com>
2015-08-31 13:49:25 +0900

    Revert "Fixed https://github.com/google/personfinder/issues/85"

--- app/flag_note.py
+++ app/flag_note.py
@@ -64,7 +64,7 @@ class Handler(utils.BaseHandler):
 
             person = model.Person.get(self.repo, note.person_record_id)
             if person:
-                person.update_latest_status(note)
+                person.update_latest_status()
 
             self.redirect(self.get_url('/view', id=note.person_record_id,
                                        signature=self.params.signature))
