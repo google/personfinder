e8f08f3778cdea09204c8dbea6da05cebf0fbe4a
hashimo <mikahashimoto@google.com>
2015-09-07 12:33:56 +0900

    add test

---
+++ tests/test_script_variant.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python2.7
+# encoding: utf-8
+"""Tests for script_variant.py"""
+import unittest
+
+import script_variant
+
+class ScriptVariantTests(unittest.TestCase):
+    def test_apply_script_variant(self):
+        assert script_variant.apply_script_variant(u'四条 貴音') == u'四条 貴音'
+        assert script_variant.apply_script_variant(u'三浦あずさ king') == u'三浦azusa king'
+    
+    def test_translate_languages_to_roman(self):
+        assert script_variant.translate_languages_to_roman(u'吉澤') == u'吉澤'
+        assert script_variant.translate_languages_to_roman(u'Reporter') == u'Reporter'
