0983d61bebc3179fac7da843ae130f4085174c35
hashimo <mikahashimoto@google.com>
2015-09-30 15:59:30 +0900

    applied comment

--- tools/make_dictionary_file.py
+++ tools/make_dictionary_file.py
@@ -3,6 +3,16 @@ Makes dictionary file from mozc
 (https://github.com/google/mozc/tree/master/src/data/dictionary_oss)
 """
 
+def check_number(id1, id2, numbers):
+    """
+    checks id1 == id2 == numbers
+    """
+    for number in numbers:
+        if id1 != id2:
+            return False
+    return True
+
+
 def make_jp_name_dictionary(input_file_names):
     """
     Makes japanese name dictionary.
@@ -10,29 +20,29 @@ def make_jp_name_dictionary(input_file_names):
     Args:
         input_file_names: a list of file names
 
-    Out put format:
+    Output format:
         kanji '\t' yomigana(hiragana) '\n'
         kanji '\t' yomigana(hiragana) '\n' ...
     """
 
     yomigana_list = []
     for input_file_name in input_file_names:
+        input_file = open(input_file_name, 'r')
+        for line in input_file:
+            line = line.rstrip()
+            splited_line = line.split('\t')
+            id1 = int(splited_line[1])
+            id2 = int(splited_line[2])
+            # 1845: id for given name
+            # 1846: id for family name
+            numbers = [1845, 1846]
+            if (check_number(id1, id2, numbers)):
+                yomigana = splited_line[0]
+                kanji = splited_line[4]
+                yomigana_list.append(kanji + '\t' + yomigana + '\n')
 
-        with open(input_file_name, 'r') as f:
-            for line in f:
-                line = line.rstrip()
-                splited_line = line.split('\t')
-                left = int(splited_line[1])
-                right = int(splited_line[2])
-
-                if ((left == 1845 and right == 1845) or
-                    (left == 1846 and right == 1846)):
-                    yomigana = splited_line[0]
-                    kanji = splited_line[4]
-                    yomigana_list.append(kanji + '\t' + yomigana + '\n')
-
-    with open('japanese_name_dict.txt', 'w') as f:
-        f.writelines(yomigana_list)
+    output_file = open('japanese_name_dict.txt', 'w')
+    output_file.writelines(yomigana_list)
 
 
 def make_jp_location_dictionary(input_file_names):
@@ -47,19 +57,18 @@ def make_jp_location_dictionary(input_file_names):
     """
     yomigana_list = []
     for input_file_name in input_file_names:
-        with open(input_file_name, 'r') as f:
-            for line in f:
-                line = line.rstrip()
-                splited_line = line.split('\t')
-                left = int(splited_line[1])
-                right = int(splited_line[2])
-                if ((left == 1847 and right == 1847) or
-                    (left == 1848 and right == 1848) or
-                    (left == 1849 and right == 1849) or
-                    (left == 1850 and right == 1850)):
-                    yomigana = splited_line[0]
-                    kanji = splited_line[4]
-                    yomigana_list.append(kanji + '\t' + yomigana + '\n')
+        input_file = open(input_file_name, 'r')
+        for line in f:
+            line = line.rstrip()
+            splited_line = line.split('\t')
+            id1 = int(splited_line[1])
+            id2 = int(splited_line[2])
+            # 1847 ~ 1850: ids for location
+            numbers = [1847, 1848, 1849, 1850]
+            if (check_number(id1, id2, numbers)):
+                yomigana = splited_line[0]
+                kanji = splited_line[4]
+                yomigana_list.append(kanji + '\t' + yomigana + '\n')
 
-    with open('jp_location_dict.txt', 'w') as f:
-        f.writelines(yomigana_list)
+    output_file = open('jp_location_dict.txt', 'w')
+    output_file.writelines(yomigana_list)
