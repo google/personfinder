529d2abc1c9f4a97d3584881059ff0b038d24e0c
hashimo <mikahashimoto@google.com>
2015-10-02 15:23:46 +0900

    applied comment

--- tools/make_dictionary_file.py
+++ tools/make_dictionary_file.py
@@ -1,12 +1,16 @@
+"""
+Makes dictionary file from mozc
+(https://github.com/google/Mozc dictionary/tree/master/src/data/dictionary_oss/
+dictionary*)
+
+command line example: tools/make_dictionary_file.py dictionary.txt ...
+
+The path to the output file: app/jp_location_dict.txt, app/japanese_name_dict.txt
+"""
+
 #!/usr/bin/python2.7
 
 import sys
-"""
-Makes dictionary file from mozc
-(https://github.com/google/mozc/tree/master/src/data/dictionary_oss/dictionary*)
-
-The path to the output file: /personfinder/app/
-"""
 
 def check_number(id1, id2, numbers):
     """
@@ -51,7 +55,7 @@ def make_jp_name_dictionary(input_file_names):
     # 1846: id for family name in mozc dictionary
     # if we want to get given name, we need to get results (id1 == id2 == 1845)
     numbers = [1845, 1846]
-    make_dictionary(input_file_names, 'japanese_name_dict.txt', numbers)
+    make_dictionary(input_file_names, 'app/japanese_name_dict.txt', numbers)
 
 
 def make_jp_location_dictionary(input_file_names):
@@ -62,8 +66,7 @@ def make_jp_location_dictionary(input_file_names):
     # if we want to get location, we need to get results
     # (id1 == id2 == 1847 ~ 1850)
     numbers = [1847, 1848, 1849, 1850]
-    make_dictionary(input_file_names, 'jp_location_dict.txt', numbers)
-
+    make_dictionary(input_file_names, 'app/jp_location_dict.txt', numbers)
 
 def main():
     dictionaries = sys.argv
