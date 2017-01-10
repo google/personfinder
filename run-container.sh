44375a83af0f6befc265c0f1039a546d0f761312
TortueMat <mathieu.tortuyaux@gmail.com>
2017-01-10 15:47:02 -0500

    Update run-container.sh

--- run-container.sh
+++ run-container.sh
@@ -6,7 +6,7 @@ CONTAINER_NAME=$2
 PRJ_DIR=$(dirname $(pwd))
 
 if [ ! -z ${CONTAINER_NAME} ]; then
-    docker run -p 8000:8000 -p 8001:8001 -p 49532:8002 --name ${CONTAINER_NAME} -it ${IMAGE_NAME} bash
+    docker run -p 8000:8000 -p 8001:8001 -p 49532:8002  -v ${PRJ_DIR}/personfinder/app:/opt/personfinder/app -v ${PRJ_DIR}/personfinder/tools:/opt/personfinder/tools -v ${PRJ_DIR}/personfinder/tests:/opt/personfinder/tests --name ${CONTAINER_NAME} -it ${IMAGE_NAME} bash
 else
     docker run -p 8000:8000 -p 8001:8001 -p 49532:8002 -it ${IMAGE_NAME} bash
 fi
