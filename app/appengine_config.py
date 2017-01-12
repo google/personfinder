82d10f1a6f5a17b0a5cdaca16af555a9bd5809d2
TortueMat <mathieu.tortuyaux@gmail.com>
2017-01-11 23:28:56 -0500

    Update appengine_config.py

--- app/appengine_config.py
+++ app/appengine_config.py
@@ -5,5 +5,8 @@ import os
 
 from google.appengine.ext import vendor
 
-vendor.add(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib'))
+try:
+    vendor.add(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib'))
 
+except: 
+    pass
