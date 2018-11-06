d4f8d4cffc92ad49913c4d903f305fae73e66d20
Ashutosh Narayan <aashutoshnarayan@gmail.com>
2018-11-06 15:53:13 +0530

    move files from utils to handlers

--- tools/create_subdirectory.py
+++ tools/create_subdirectory.py
@@ -5,13 +5,14 @@
 # Create subdirectory under app/ directory
 
 def main():
- import os,sys
+ import os,sys,glob,time
 
  dirName1 = '../app/handlers'
  dirName2 = '../app/utils'
  dirName3 = '../app/data'
  file1 = '../app/japanese_name_location_dict.txt'
  file2 = '../app/chinese_family_name_dict.txt'
+ file3 = '../app/utils/utils.py'
 
  #Create subdirectories if they don't exist
  if not os.path.exists(dirName1):
@@ -33,8 +34,14 @@ def main():
      print ("Directory", dirName3, "already exists")
 
  # Move all the BaseHandlers to handlers subdirectory
+ 
  os.system('grep -Ril "BaseHandler" ../app/ | xargs mv -t ../app/handlers/')
  os.system('grep -Ril --exclude-dir=../app/handlers/ "utils.BaseHandler" ../app/ | xargs mv -t ../app/utils/')
+
+ # Move all files to handlers except few from utils directory 
+
+ os.system('find ../app/utils/* ! -name utils.py -exec mv -t ../app/handlers/ {} +')
+
  if os.path.exists(file1):
      os.system('mv ../app/japanese_name_location_dict.txt ../app/data/')
  else:
