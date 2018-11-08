43769b95bceb2c36a73b176ab286b868ea580ae8
Hiroshi Ichikawa <gimite@gmail.com>
2018-11-08 17:20:31 +0900

    Fix formatting.

--- tools/update_messages.py
+++ tools/update_messages.py
@@ -156,7 +156,8 @@ if __name__ == '__main__':
     old_msgids = set(message.id for message in old_template)
 
     # Run makemessages to update the English .po file.
-    django_admin('makemessages', '-l', 'en', '-i', 'vendors', *makemessages_args)
+    django_admin(
+        'makemessages', '-l', 'en', '-i', 'vendors', *makemessages_args)
 
     # Get the set of messages in the new English .po file.
     new_template = read_po(open(en_filename))
