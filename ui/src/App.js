c3738dc01299d1068023414c2a677257442b536c
Nick Worden <nworden@google.com>
2019-01-14 14:33:27 -0500

    Clean up page component names.
    Make names match URLs and put them in a pages directory.

--- ui/src/App.js
+++ ui/src/App.js
@@ -2,19 +2,19 @@ import React from "react";
 import {injectIntl} from 'react-intl';
 import {BrowserRouter, Route} from "react-router-dom";
 
-import AddPerson from "./AddPerson.js";
-import GlobalHome from "./GlobalHome.js";
-import RepoHome from "./RepoHome.js";
-import RepoResults from "./RepoResults.js";
-import View from "./View.js";
+import Create from "./pages/Create.js";
+import GlobalHome from "./pages/GlobalHome.js";
+import RepoHome from "./pages/RepoHome.js";
+import Results from "./pages/Results.js";
+import View from "./pages/View.js";
 
 const App = () => (
   <BrowserRouter>
     <div>
       <Route exact path="/" component={GlobalHome} />
       <Route exact path="/:repoId" component={RepoHome} />
-      <Route exact path="/:repoId/create" component={AddPerson} />
-      <Route exact path="/:repoId/results" component={RepoResults} />
+      <Route exact path="/:repoId/create" component={Create} />
+      <Route exact path="/:repoId/results" component={Results} />
       <Route exact path="/:repoId/view" component={View} />
     </div>
   </BrowserRouter>
