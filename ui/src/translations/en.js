fa7abae9ef5b2d73c2fe941c8eb41ff220c1b44e
Nick Worden <nworden@google.com>
2019-01-17 14:39:47 -0500

    Changes from review.

--- ui/src/translations/en.js
+++ ui/src/translations/en.js
@@ -1,2 +1,7 @@
+/**
+ * The English translation file has to exist to avoid complicating things for
+ * index.js, but it doesn't need to actually have any translations, because that
+ * would just be redundant with the default messages (which are all in English).
+ */
 module.exports = {
 };
--- ui/src/translations/es.js
+++ ui/src/translations/es.js
@@ -1,3 +1,6 @@
+/**
+ * This file is just for temporary testing use.
+ */
 module.exports = {
   'GlobalHome.howDoesItWork': '¿Como funciona?',
   'RepoCard.repoRecordCount': 'Seguimiento de {recordCount} registros.',
