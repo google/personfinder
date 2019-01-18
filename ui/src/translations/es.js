92c8600ab06ca41b93eab1b8a880c89a53b2383e
Nick Worden <9059304+nworden@users.noreply.github.com>
2019-01-17 19:21:51 -0500

    Translation files for development. (#467)

---
+++ ui/src/translations/es.js
@@ -0,0 +1,7 @@
+/**
+ * This file is just for temporary testing use.
+ */
+module.exports = {
+  'GlobalHome.howDoesItWork': '¿Como funciona?',
+  'RepoCard.repoRecordCount': 'Seguimiento de {recordCount} registros.',
+};
