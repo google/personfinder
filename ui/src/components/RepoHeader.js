da38e7df4f27083d57151d751154dc52f6777de2
Nick Worden <nworden@google.com>
2019-01-21 14:40:32 -0500

    Fix quotes, copyright notices.

--- ui/src/components/RepoHeader.js
+++ ui/src/components/RepoHeader.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import React, {Component} from 'react';
 import {FormattedHTMLMessage, defineMessages, injectIntl} from 'react-intl';
 import {Link} from "react-router-dom";
@@ -14,17 +30,17 @@ const messages = defineMessages({
 class RepoHeader extends Component {
   render() {
     return (
-      <div id="repoheader">
-        <div id="repoheader-backbutton">
-          <Link to={"/" + this.props.repoInfo.repoId}>
-            <div><img src="/static/icons/maticon_arrow_back.svg" /></div>
+      <div id='repoheader'>
+        <div id='repoheader-backbutton'>
+          <Link to={'/' + this.props.repoInfo.repoId}>
+            <div><img src='/static/icons/maticon_arrow_back.svg' /></div>
           </Link>
         </div>
-        <div id="repoheader-info">
-          <p className="mdc-typography--subtitle1">
+        <div id='repoheader-info'>
+          <p className='mdc-typography--subtitle1'>
             <FormattedHTMLMessage {...messages.productName} />
           </p>
-          <p className="mdc-typography--subtitle2">
+          <p className='mdc-typography--subtitle2'>
             {this.props.repoInfo.title}
           </p>
         </div>
