a58d9bf8e1d04bdf4977a41f21164d4f05d11d1d
Nick Worden <nworden@google.com>
2019-01-21 14:38:00 -0500

    Fix quotes, copyright notices.

--- ui/src/components/SearchBar.js
+++ ui/src/components/SearchBar.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import React, {Component} from 'react';
 import {defineMessages, injectIntl} from 'react-intl';
 import {withRouter} from 'react-router-dom';
@@ -15,7 +31,7 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: (props.initialValue == null ? "" : props.initialValue)};
+      value: (props.initialValue == null ? '' : props.initialValue)};
     this.handleKeyDown = this.handleKeyDown.bind(this);
   }
 
@@ -31,7 +47,7 @@ class SearchBar extends Component {
       <TextField
         label={this.props.intl.formatMessage(messages.searchForAPerson)}
         outlined
-        className="searchbar"
+        className='searchbar'
       >
         <Input
           value={this.state.value}
