55079fcc129705bdc16108f35e1712cd7369cf19
Nick Worden <nworden@google.com>
2019-01-22 10:25:51 -0500

    Make PFNotchedOutline a simple function.

--- ui/src/components/PFNotchedOutline.js
+++ ui/src/components/PFNotchedOutline.js
@@ -24,17 +24,13 @@ import React, {Component} from 'react';
  * pretty sparse and b) I'm not sure Material supports using their
  * NotchedOutline with anything other than TextField and Select components.
  */
-class PFNotchedOutline extends Component {
-  render() {
-    return (
-      <div className='pf-notchedoutline'>
-        <label className='mdc-typography--body1 pf-notchedoutline-label'>
-          {this.props.label}
-        </label>
-        <div className='pf-notchedoutline-content'>{this.props.children}</div>
-      </div>
-    );
-  }
-}
+const PFNotchedOutline = () => (
+  <div className='pf-notchedoutline'>
+    <label className='mdc-typography--body1 pf-notchedoutline-label'>
+      {this.props.label}
+    </label>
+    <div className='pf-notchedoutline-content'>{this.props.children}</div>
+  </div>
+);
 
 export default PFNotchedOutline;
