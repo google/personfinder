5895c931eb58c2c4d616663726395c9f6f5050bf
Nick Worden <9059304+nworden@users.noreply.github.com>
2019-01-22 10:38:53 -0500

    Component for general-purpose notched outline. (#484)
    * Component for general-purpose notched outline.
    
    The mocks call for a notched outline around sections of the create form.
    However, the Material libraries only seem to have support for notched
    outlines on input components, so, at least for now, I think we need our
    own component for the section outlines.
    
    * Copyright notices for notched outline files.
    
    * Make PFNotchedOutline a simple function.

---
+++ ui/src/components/PFNotchedOutline.js
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, {Component} from 'react';
+
+/**
+ * A component for wrapping something in a notched outline.
+ *
+ * We may be able to get rid of this at some point and use a component built by
+ * the Material team, but a) the documentation for the React NotchedOutline is
+ * pretty sparse and b) I'm not sure Material supports using their
+ * NotchedOutline with anything other than TextField and Select components.
+ */
+const PFNotchedOutline = () => (
+  <div className='pf-notchedoutline'>
+    <label className='mdc-typography--body1 pf-notchedoutline-label'>
+      {this.props.label}
+    </label>
+    <div className='pf-notchedoutline-content'>{this.props.children}</div>
+  </div>
+);
+
+export default PFNotchedOutline;
