4833c75f65d048a3cfe6f9556d3e8bb1f23d0c3c
Nick Worden <nworden@google.com>
2019-01-22 19:05:50 -0500

    Component for end bar header.

---
+++ ui/src/components/EndBarHeader.js
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, {Component} from 'react';
+
+/*
+ * A component for headers with bars on the ends.
+ *
+ * They look something like this:
+ *      ----- Title or whatever -----
+ */
+class EndBarHeader extends Component {
+  render() {
+    return (
+      <div className='endbars-headerline-wrapper' dir='ltr'>
+        <span className='mdc-typography--overline endbars-headerline'>
+          {this.props.children}
+        </span>
+      </div>
+    );
+  }
+}
+
+export default EndBarHeader;
