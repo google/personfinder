46f7118dc68656918704b66522a1f4a7fac3ca51
Nick Worden <9059304+nworden@users.noreply.github.com>
2019-01-26 18:40:04 -0500

    Fix mocking in tests.

--- tests/test_searcher.py
+++ tests/test_searcher.py
@@ -1,4 +1,4 @@
-from mock import patch
+from mock import patch, MagicMock
 import unittest
 
 from search.searcher import Searcher
@@ -14,66 +14,74 @@ class SearcherTests(unittest.TestCase):
     FULLTEXT_RETURN_VALUE = ['full-text return value']
     INDEXING_RETURN_VALUE = ['indexing return value']
 
-    def setUp(self):
-        pass
-
-    @patch('external_search.search')
-    def test_external_search_backend_results(self, external_search_mock):
+    def test_external_search_backend_results(self):
         """Return external search backend results when available."""
-        external_search_mock.return_value = (
-            SearcherTests.EXTERNAL_SEARCH_RETURN_VALUE)
-        external_search_value = 'abc external search'
-        searcher = Searcher(
-            SearcherTests.REPO_NAME, external_search_value, False,
-            SearcherTests.MAX_RESULTS)
-        assert (searcher.search({'name': 'matt'}) ==
+        with patch('external_search.search') as external_search_mock, \
+             patch('full_text_search.search') as full_text_search_mock, \
+             patch('indexing.search') as indexing_mock:
+            full_text_search_mock.return_value = []
+            indexing_mock.return_value = []
+            external_search_mock.return_value = (
                 SearcherTests.EXTERNAL_SEARCH_RETURN_VALUE)
-        assert 1 == len(external_search_mock.call_args_list)
-        call_args, _ = external_search_mock.call_args_list[0]
-        assert call_args[0] == SearcherTests.REPO_NAME
-        assert call_args[1].query == 'matt'
-        assert call_args[2] == SearcherTests.MAX_RESULTS
-        assert call_args[3] == external_search_value
+            external_search_value = 'abc external search'
+            searcher = Searcher(
+                SearcherTests.REPO_NAME,
+                external_search_backends=external_search_value,
+                enable_fulltext_search=False,
+                max_results=SearcherTests.MAX_RESULTS)
+            assert (searcher.search({'name': 'matt'}) ==
+                    SearcherTests.EXTERNAL_SEARCH_RETURN_VALUE)
+            assert 1 == len(external_search_mock.call_args_list)
+            call_args, _ = external_search_mock.call_args_list[0]
+            assert call_args[0] == SearcherTests.REPO_NAME
+            assert call_args[1].query == 'matt'
+            assert call_args[2] == SearcherTests.MAX_RESULTS
+            assert call_args[3] == external_search_value
 
-    @patch('external_search.search')
-    @patch('indexing.search')
-    def test_indexing_results(self, external_search_mock, indexing_mock):
+    def test_indexing_results(self):
         """Fall back to indexing.search results.
 
         When external search backends don't return any results and full-text
         search is disabled, fall back to indexing.search.
         """
-        external_search_mock.return_value = []
-        indexing_mock.return_value = SearcherTests.INDEXING_RETURN_VALUE
-        searcher = Searcher(
-            SearcherTests.REPO_NAME, 'any value', False,
-            SearcherTests.MAX_RESULTS)
-        assert (searcher.search({'name': 'matt'}) ==
-                SearcherTests.INDEXING_RETURN_VALUE)
-        assert 1 == len(indexing_mock.call_args_list)
-        call_args, _ = indexing_mock.call_args_list[0]
-        assert call_args[0] == SearcherTests.REPO_NAME
-        assert call_args[1].query == 'matt'
-        assert call_args[2] == SearcherTests.MAX_RESULTS
+        with patch('external_search.search') as external_search_mock, \
+             patch('full_text_search.search') as full_text_search_mock, \
+             patch('indexing.search') as indexing_mock:
+            external_search_mock.return_value = []
+            indexing_mock.return_value = SearcherTests.INDEXING_RETURN_VALUE
+            searcher = Searcher(
+                SearcherTests.REPO_NAME,
+                external_search_backends='any value',
+                enable_fulltext_search=False,
+                max_results=SearcherTests.MAX_RESULTS)
+            assert (searcher.search({'name': 'matt'}) ==
+                    SearcherTests.INDEXING_RETURN_VALUE)
+            assert 1 == len(indexing_mock.call_args_list)
+            call_args, _ = indexing_mock.call_args_list[0]
+            assert call_args[0] == SearcherTests.REPO_NAME
+            assert call_args[1].query == 'matt'
+            assert call_args[2] == SearcherTests.MAX_RESULTS
 
-    @patch('external_search.search')
-    @patch('full_text_search.search')
-    def test_full_text_search_results(
-        self, external_search_mock, full_text_search_mock):
+    def test_full_text_search_results(self):
         """Fall back to full_text_search.search results.
 
         When external search backends don't return any results and full-text
         search is enabled, fall back to full_text_search.search.
         """
-        external_search_mock.return_value = []
-        full_text_search_mock.return_value = SearcherTests.FULLTEXT_RETURN_VALUE
-        searcher = Searcher(
-            SearcherTests.REPO_NAME, 'any value', False,
-            SearcherTests.MAX_RESULTS)
-        assert (searcher.search({'name': 'matt'}) ==
-                SearcherTests.FULLTEXT_RETURN_VALUE)
-        assert 1 == len(full_text_search_mock.call_args_list)
-        call_args, _ = full_text_search_mock.call_args_list[0]
-        assert call_args[0] == SearcherTests.REPO_NAME
-        assert call_args[1].query == 'matt'
-        assert call_args[2] == SearcherTests.MAX_RESULTS
+        with patch('external_search.search') as external_search_mock, \
+             patch('full_text_search.search') as full_text_search_mock, \
+             patch('indexing.search') as indexing_mock:
+            external_search_mock.return_value = []
+            full_text_search_mock.return_value = SearcherTests.FULLTEXT_RETURN_VALUE
+            searcher = Searcher(
+                SearcherTests.REPO_NAME,
+                external_search_backends='any value',
+                enable_fulltext_search=True,
+                max_results=SearcherTests.MAX_RESULTS)
+            assert (searcher.search({'name': 'matt'}) ==
+                    SearcherTests.FULLTEXT_RETURN_VALUE)
+            assert 1 == len(full_text_search_mock.call_args_list)
+            call_args, _ = full_text_search_mock.call_args_list[0]
+            assert call_args[0] == SearcherTests.REPO_NAME
+            assert call_args[1] == {'name': 'matt'}
+            assert call_args[2] == SearcherTests.MAX_RESULTS
