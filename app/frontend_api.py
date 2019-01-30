289bc2ae7a39412e04b97785ba438da713890bd5
Hiroshi Ichikawa <gimite@gmail.com>
2019-01-30 17:38:00 +0900

    Implement view page (partially).

--- app/frontend_api.py
+++ app/frontend_api.py
@@ -71,7 +71,18 @@ class Person(FrontendApiHandler):
 
     def get(self):
         # TODO: implement this
-        self._return_json({'name': 'Hard-coded placeholder'})
+        self._return_json({
+          'name': 'Hard-coded placeholder',
+          'sex': 'male',
+          'age': '42',
+          'home_street': 'Amphitheatre Pkwy',
+          'home_city': 'Mountain View',
+          'home_state': 'CA',
+          'home_country': 'U.S.',
+          'description': '''
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          ''',
+        })
 
 
 class Create(FrontendApiHandler):
