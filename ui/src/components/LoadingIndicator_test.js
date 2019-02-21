058e0421560f88f40cbf5cefe58abf5927ff575b
Nick Worden <nworden@google.com>
2019-02-21 12:40:12 -0500

    LoadingIndicator test.

---
+++ ui/src/components/LoadingIndicator_test.js
@@ -0,0 +1,16 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
+
+import LoadingIndicator from './LoadingIndicator';
+import {mountWithIntl} from '../testing/enzyme-intl-helper';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+test('loading indicator should say loading', () => {
+  const wrapper = mountWithIntl(
+    <LoadingIndicator />
+  );
+  expect(wrapper.find('span').text()).toBe('Loading...');
+  wrapper.unmount();
+});
