9bc5c1509b2fc7d4db9b49c67d85368482bd9fda
Nick Worden <nworden@google.com>
2019-02-27 14:14:12 -0500

    Rework page tests.

--- ui/src/pages/GlobalHome_test.js
+++ ui/src/pages/GlobalHome_test.js
@@ -7,7 +7,7 @@ import {MemoryRouter} from 'react-router';
 
 import GlobalHome from './GlobalHome';
 import {mountWithIntl} from '../testing/enzyme-intl-helper';
-import {runPageTest} from '../testing/utils';
+import {runPageTest, flushPromises} from '../testing/utils';
 
 Enzyme.configure({adapter: new Adapter()});
 
@@ -38,70 +38,65 @@ describe('testing GlobalHome', () => {
   });
 
   test('repo cards are rendered', (done) => {
-    runPageTest(
-        () => {
-          return setupPageWrapper();
-        },
-        (wrapper, _) => {
-          const repoCardTitles = wrapper.find('h5.repocard-title');
-          expect(repoCardTitles.length).toBe(7);
-          const expectedTitles = [
-              'Haiti', 'Japan', 'Albany', 'Seattle', 'Montreal', 'Belgium',
-              'Boise'];
-          for (let i = 0; i < expectedTitles.length; i++) {
-            expect(repoCardTitles.at(i).text()).toBe(expectedTitles[i]);
-          }
-        },
-        done);
+    const [wrapper, _] = setupPageWrapper();
+    flushPromises().then(() => {
+      wrapper.update();
+      const repoCardTitles = wrapper.find('h5.repocard-title');
+      expect(repoCardTitles.length).toBe(7);
+      const expectedTitles = [
+          'Haiti', 'Japan', 'Albany', 'Seattle', 'Montreal', 'Belgium',
+          'Boise'];
+      for (let i = 0; i < expectedTitles.length; i++) {
+        expect(repoCardTitles.at(i).text()).toBe(expectedTitles[i]);
+      }
+      wrapper.unmount();
+      done();
+    });
   });
 
   test('assigns display categories to repos', (done) => {
-    runPageTest(
-        () => {
-          return setupPageWrapper();
-        },
-        (wrapper, _) => {
-          const expectedDisplayCategories = [0, 1, 2, 3, 4, 0, 1];
-          for (let i = 0; i < expectedDisplayCategories.length; i++) {
-            expect(wrapper.find('GlobalHome').state().repos[i].displayCategory)
-                .toBe(expectedDisplayCategories[i]);
-          }
-        },
-        done);
+    const [wrapper, _] = setupPageWrapper();
+    flushPromises().then(() => {
+      wrapper.update();
+      const expectedDisplayCategories = [0, 1, 2, 3, 4, 0, 1];
+      for (let i = 0; i < expectedDisplayCategories.length; i++) {
+        expect(wrapper.find('GlobalHome').state().repos[i].displayCategory)
+            .toBe(expectedDisplayCategories[i]);
+      }
+      wrapper.unmount();
+      done();
+    });
   });
 
   test('repo cards point to the right place', (done) => {
-    runPageTest(
-        () => {
-          return setupPageWrapper();
-        },
-        (wrapper, history) => {
-          const repoCardImages = wrapper.find('.repocard-image');
-          expect(repoCardImages.length).toBe(7);
-          repoCardImages.at(1).simulate('click');
-          expect(history.entries.length).toBe(2);
-          expect(history.entries[1].pathname).toBe('/japan');
-        },
-        done);
+    const [wrapper, history] = setupPageWrapper();
+    flushPromises().then(() => {
+      wrapper.update();
+      const repoCardImages = wrapper.find('.repocard-image');
+      expect(repoCardImages.length).toBe(7);
+      repoCardImages.at(1).simulate('click');
+      expect(history.entries.length).toBe(2);
+      expect(history.entries[1].pathname).toBe('/japan');
+      wrapper.unmount();
+      done();
+    });
   });
 
   test('snapshot test for GlobalHome', (done) => {
-    runPageTest(
-        () => {
-          // We don't use setupPageWrapper here because we need to avoid passing
-          // a history object: it will generate random keys that mess up the
-          // snapshot.
-          fetch.mockResponseOnce(JSON.stringify(REPO_DATA));
-          const wrapper = mountWithIntl(
-            <MemoryRouter>
-              <GlobalHome />
-            </MemoryRouter>
-          );
-          return [wrapper, null];
-        },
-        (wrapper, _) => {
-          expect(toJson(wrapper.find(GlobalHome).at(0))).toMatchSnapshot();
-        },
-        done);
+    // We don't use setupPageWrapper here because we need to avoid passing
+    // a history object: it will generate random keys that mess up the
+    // snapshot.
+    fetch.mockResponseOnce(JSON.stringify(REPO_DATA));
+    const wrapper = mountWithIntl(
+      <MemoryRouter>
+        <GlobalHome />
+      </MemoryRouter>
+    );
+    flushPromises().then(() => {
+      wrapper.update();
+      expect(toJson(wrapper.find(GlobalHome).at(0))).toMatchSnapshot();
+      wrapper.unmount();
+      done();
+    });
   });
 });
--- ui/src/testing/utils.js
+++ ui/src/testing/utils.js
@@ -1,27 +1,11 @@
 /**
- * A utility function to help run unit tests for page components.
+ * Returns a promise to run resolve after existing promises are flushed.
  *
- * runPageTest will first call setupFunc to set up the page component, which is
- * expected to return an array of two items: the Enzyme wrapper and any data
- * that the test function needs. Next, runPageFunc will call update() on the
- * Enzyme wrapper, and then call the testFunc with wrapper and the data from
- * setupFunc. Lastly, it will unmount the wrapper and call the doneFunc to clean
- * up.
- *
- * @param {function} doneFunc - The done function from Jest.
- * @param {function} setupFunc - A function to set up the component under test.
- * Should return an array of two items: the Enzyme wrapper for the page
- * component, and any data the test function needs.
- * @param {function} testFunc - A function to test the component. Will be passed
- * two parameters: the Enzyme wrapper, and the other data returned from the
- * setup function.
+ * Long story short, we can't properly test components until componentDidMount
+ * has run, but, when componentDidMount has async calls (e.g., to fetch data and
+ * then use it to set state), React/Enzyme leaves us without any good ways to
+ * wait for componentDidMount. We use setImmediate to get around this.
  */
-export function runPageTest(setupFunc, testFunc, doneFunc = () => {}) {
-  const [wrapper, data] = setupFunc();
-  setImmediate(() => {
-    wrapper.update();
-    testFunc(wrapper, data);
-    wrapper.unmount();
-    doneFunc();
-  });
-}
+export function flushPromises() {
+  return new Promise((res) => setImmediate(res));
+};
