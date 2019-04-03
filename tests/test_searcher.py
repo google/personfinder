f879e1a06cd4929dd05224d9a467c7634beebccc
Nick Worden <nworden@google.com>
2019-04-03 19:36:11 -0400

    Make pylint and yapf happy with test_searcher.py.

--- tests/test_searcher.py
+++ tests/test_searcher.py
@@ -1,10 +1,15 @@
-from mock import patch, MagicMock
+"""Tests for the Searcher."""
+
 import unittest
+
+import mock
 
 from search.searcher import Searcher
 
 
 class SearcherTests(unittest.TestCase):
+    # pylint: disable=no-self-use
+    """Test cases for searcher.Searcher."""
 
     # Argument values
     REPO_NAME = 'haiti'
@@ -15,33 +20,31 @@ class SearcherTests(unittest.TestCase):
 
     def test_full_text_search_results(self):
         """Use full_text_search.search results when enabled."""
-        with patch('full_text_search.search') as full_text_search_mock, \
-             patch('indexing.search') as indexing_mock:
+        with mock.patch('full_text_search.search') as full_text_search_mock:
             full_text_search_mock.return_value = SearcherTests.FULLTEXT_RETURN_VALUE
             searcher = Searcher(
                 SearcherTests.REPO_NAME,
                 enable_fulltext_search=True,
                 max_results=SearcherTests.MAX_RESULTS)
-            assert (searcher.search('matt') ==
-                    SearcherTests.FULLTEXT_RETURN_VALUE)
+            assert (
+                searcher.search('matt') == SearcherTests.FULLTEXT_RETURN_VALUE)
             assert len(full_text_search_mock.call_args_list) == 1
             call_args, _ = full_text_search_mock.call_args_list[0]
             assert call_args[0] == SearcherTests.REPO_NAME
             assert call_args[1] == {'name': 'matt'}
             assert call_args[2] == SearcherTests.MAX_RESULTS
 
-    def test_full_text_search_results_with_location(self):
+    def test_full_text_results_with_loc(self):
         """Use full_text_search.search results when enabled, including location.
         """
-        with patch('full_text_search.search') as full_text_search_mock, \
-             patch('indexing.search') as indexing_mock:
+        with mock.patch('full_text_search.search') as full_text_search_mock:
             full_text_search_mock.return_value = SearcherTests.FULLTEXT_RETURN_VALUE
             searcher = Searcher(
                 SearcherTests.REPO_NAME,
                 enable_fulltext_search=True,
                 max_results=SearcherTests.MAX_RESULTS)
-            assert (searcher.search('matt', 'schenectady') ==
-                    SearcherTests.FULLTEXT_RETURN_VALUE)
+            assert (searcher.search(
+                'matt', 'schenectady') == SearcherTests.FULLTEXT_RETURN_VALUE)
             assert len(full_text_search_mock.call_args_list) == 1
             call_args, _ = full_text_search_mock.call_args_list[0]
             assert call_args[0] == SearcherTests.REPO_NAME
@@ -53,35 +56,33 @@ class SearcherTests(unittest.TestCase):
 
         When full-text search is disabled, fall back to indexing.search.
         """
-        with patch('full_text_search.search') as full_text_search_mock, \
-             patch('indexing.search') as indexing_mock:
+        with mock.patch('indexing.search') as indexing_mock:
             indexing_mock.return_value = SearcherTests.INDEXING_RETURN_VALUE
             searcher = Searcher(
                 SearcherTests.REPO_NAME,
                 enable_fulltext_search=False,
                 max_results=SearcherTests.MAX_RESULTS)
-            assert (searcher.search('matt') ==
-                    SearcherTests.INDEXING_RETURN_VALUE)
+            assert (
+                searcher.search('matt') == SearcherTests.INDEXING_RETURN_VALUE)
             assert len(indexing_mock.call_args_list) == 1
             call_args, _ = indexing_mock.call_args_list[0]
             assert call_args[0] == SearcherTests.REPO_NAME
             assert call_args[1].query == 'matt'
             assert call_args[2] == SearcherTests.MAX_RESULTS
 
-    def test_indexing_results_with_location(self):
+    def test_indexing_results_with_loc(self):
         """Fall back to indexing.search results, including a location value.
 
         When full-text search is disabled, fall back to indexing.search.
         """
-        with patch('full_text_search.search') as full_text_search_mock, \
-             patch('indexing.search') as indexing_mock:
+        with mock.patch('indexing.search') as indexing_mock:
             indexing_mock.return_value = SearcherTests.INDEXING_RETURN_VALUE
             searcher = Searcher(
                 SearcherTests.REPO_NAME,
                 enable_fulltext_search=False,
                 max_results=SearcherTests.MAX_RESULTS)
-            assert (searcher.search('matt', 'schenectady') ==
-                    SearcherTests.INDEXING_RETURN_VALUE)
+            assert (searcher.search(
+                'matt', 'schenectady') == SearcherTests.INDEXING_RETURN_VALUE)
             assert len(indexing_mock.call_args_list) == 1
             call_args, _ = indexing_mock.call_args_list[0]
             assert call_args[0] == SearcherTests.REPO_NAME
