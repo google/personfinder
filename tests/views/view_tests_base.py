634585998729458dd704be8930ccf71934e17a76
Nick Worden <nworden@google.com>
2019-04-04 21:38:25 -0400

    Set up XsrfTool in ViewTestsBase.

--- tests/views/view_tests_base.py
+++ tests/views/view_tests_base.py
@@ -8,6 +8,7 @@ import django.test
 from google.appengine.ext import testbed
 
 import const
+import utils
 
 import scrape
 
@@ -23,6 +24,7 @@ class ViewTestsBase(unittest.TestCase):
         django.setup()
         django.test.utils.setup_test_environment()
         self.client = django.test.Client()
+        self.xsrf_tool = utils.XsrfTool()
 
     def tearDown(self):
         self.testbed.deactivate()
