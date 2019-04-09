2f84664a4aee9e9dfe41c3b5b8495501c2817ee5
Nick Worden <9059304+nworden@users.noreply.github.com>
2019-04-09 17:29:09 -0400

    Automatic XSRF tests.

--- app/views/admin/create_repo.py
+++ app/views/admin/create_repo.py
@@ -67,9 +67,9 @@ class AdminCreateRepoView(views.admin.base.AdminBaseView):
             HttpResponse: A redirect to the new repo's admin page.
         """
         del request, args, kwargs  # unused
-        if not (self.params.xsrf_token and self.xsrf_tool.verify_token(
-                self.params.xsrf_token, self.env.user.user_id(),
-                self.ACTION_ID)):
+        if not (self.params.get('xsrf_token') and self.xsrf_tool.verify_token(
+                    self.params.xsrf_token, self.env.user.user_id(),
+                    self.ACTION_ID)):
             return self.error(403)
         new_repo = self.params.new_repo
         model.Repo(key_name=new_repo).put()
--- tests/views/test_auto_security.py
+++ tests/views/test_auto_security.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """Access restriction tests."""
 
 import collections
@@ -21,25 +20,36 @@ import django.urls
 
 import urls
 import utils
-import views
 
 import view_tests_base
 
-PathTestInfo = collections.namedtuple('PathTestInfo', [
-    'accepts_get',
-    'accepts_post',
-    'restricted_to_admins',
-    'requires_xsrf',
-    'sample_post_data',
-    'xsrf_action_id',
-])
+PathTestInfo = collections.namedtuple(
+    'PathTestInfo',
+    [
+        # Whether the path is expected to accept GET requests.
+        'accepts_get',
+        # Whether the past is expected to accept POST requests.
+        'accepts_post',
+        # Whether the page is restricted to admins.
+        'restricted_to_admins',
+        # Whether POST requests should require an XSRF token.
+        'requires_xsrf',
+        # A dict with valid POST data, excluding an XSRF token (the tests will add,
+        # or omit, the XSRF tokens). Should be None if XSRF isn't used for the page.
+        'sample_post_data',
+        # The action ID used for XSRF tokens. Should be None if XSRF isn't used for
+        # the page.
+        'xsrf_action_id',
+    ])
 
 
 class AutoSecurityTests(view_tests_base.ViewTestsBase):
     """Tests that access restrictions are enforced."""
+    # pylint: disable=no-self-use
 
     PATH_TEST_INFO = {
-        'admin-create-repo': PathTestInfo(
+        'admin-create-repo':
+        PathTestInfo(
             accepts_get=True,
             accepts_post=True,
             restricted_to_admins=True,
@@ -48,7 +58,8 @@ class AutoSecurityTests(view_tests_base.ViewTestsBase):
                 'new_repo': 'new-hampshire',
             },
             xsrf_action_id='admin/create_repo'),
-        'admin-statistics': PathTestInfo(
+        'admin-statistics':
+        PathTestInfo(
             accepts_get=True,
             accepts_post=False,
             restricted_to_admins=True,
@@ -62,6 +73,10 @@ class AutoSecurityTests(view_tests_base.ViewTestsBase):
         self.xsrf_tool = utils.XsrfTool()
 
     def get_valid_post_data(self, path_info):
+        """Gets data for a valid POST requests.
+
+        Generates and includes a valid XSRF token when needed.
+        """
         if path_info.requires_xsrf:
             # Copy the data dict to avoid mutating the original.
             data = copy.deepcopy(path_info.sample_post_data)
@@ -71,25 +86,36 @@ class AutoSecurityTests(view_tests_base.ViewTestsBase):
             return data
         return path_info.sample_post_data
 
+    def get_paths_to_test(self, filter_func):
+        """Gets paths to test based on a filter function.
+
+        Args:
+            filter_func (function): A function that accepts a PathTestInfo tuple
+            and returns True or False to indicate if it should be included.
+
+        Returns:
+            list: A list of tuples with path names and PathTestInfo tuples.
+        """
+        return [(path_name, path_info)
+                for (path_name,
+                     path_info) in AutoSecurityTests.PATH_TEST_INFO.items()
+                if filter_func(path_info)]
+
     def test_blocked_to_non_admins(self):
         """Tests that admin-only pages aren't available to non-admins."""
         self.login(is_admin=False)
-        for (path_name, path_info
-            ) in filter(lambda (_, path_info): path_info.restricted_to_admins,
-                        AutoSecurityTests.PATH_TEST_INFO.items()):
+        for (path_name, path_info) in self.get_paths_to_test(
+                lambda path_info: path_info.restricted_to_admins):
             path = django.urls.reverse(path_name)
             if path_info.accepts_get:
                 self.assertEqual(
-                    self.client.get(path, secure=True).status_code,
-                    403,
+                    self.client.get(path, secure=True).status_code, 403,
                     'Admin-only page available to non-admin: %s' % path_name)
             if path_info.accepts_post:
                 self.assertEqual(
                     self.client.post(
-                        path,
-                        self.get_valid_post_data(path_info),
-                        secure=True).status_code,
-                    403,
+                        path, self.get_valid_post_data(path_info),
+                        secure=True).status_code, 403,
                     'Admin-only page available to non-admin: %s' % path_name)
 
     def test_available_to_admins(self):
@@ -102,48 +128,71 @@ class AutoSecurityTests(view_tests_base.ViewTestsBase):
         somehow).
         """
         self.login(is_admin=True)
-        for (path_name, path_info
-            ) in filter(lambda (_, path_info): path_info.restricted_to_admins,
-                        AutoSecurityTests.PATH_TEST_INFO.items()):
+        for (path_name, path_info) in self.get_paths_to_test(
+                lambda path_info: path_info.restricted_to_admins):
             path = django.urls.reverse(path_name)
             if path_info.accepts_get:
                 self.assertEqual(
-                    self.client.get(path, secure=True).status_code,
-                    200,
+                    self.client.get(path, secure=True).status_code, 200,
                     'GET unavailable to admins for: %s' % path_name)
             # Some POST requests might return a redirect, so just ensure they
             # don't get a 403 like non-admins do.
             if path_info.accepts_post:
                 self.assertNotEqual(
                     self.client.post(
-                        path,
-                        self.get_valid_post_data(path_info),
-                        secure=True).status_code,
-                    403,
+                        path, self.get_valid_post_data(path_info),
+                        secure=True).status_code, 403,
                     'POST unavailable to admins for: %s' % path_name)
 
     def test_other_pages_unrestricted(self):
         """Tests that non-admins can access unrestricted pages."""
         self.login(is_admin=False)
-        for (path_name, path_info
-            ) in filter(lambda (_, path_info):
-                        not path_info.restricted_to_admins,
-                        AutoSecurityTests.PATH_TEST_INFO.items()):
+        for (path_name, path_info) in self.get_paths_to_test(
+                lambda path_info: not path_info.restricted_to_admins):
             path = django.urls.reverse(path_name)
             if path_info.accepts_get:
                 self.assertEqual(
-                    self.client.get(path, secure=True).status_code,
-                    200,
+                    self.client.get(path, secure=True).status_code, 200,
                     'Unrestricted page blocked to non-admin: %s' % path_name)
             if path_info.accepts_post:
                 # Redirects are fine, just check that they don't get a 403.
                 self.assertNotEqual(
                     self.client.post(
-                        path,
-                        self.get_valid_post_data(path_info),
-                        secure=True).status_code,
-                    403,
+                        path, self.get_valid_post_data(path_info),
+                        secure=True).status_code, 403,
                     'Unrestricted page blocked to non-admin: %s' % path_name)
+
+    def test_missing_xsrf_token(self):
+        """Tests that, if XSRF is required, POSTs without a token are rejected.
+        """
+        self.login(is_admin=True)
+        for (path_name, path_info) in self.get_paths_to_test(
+                lambda path_info: path_info.requires_xsrf):
+            # Just in case someone accidentally included an XSRF token in the
+            # PathTestInfo tuple.
+            assert 'xsrf_token' not in path_info.sample_post_data
+            path = django.urls.reverse(path_name)
+            self.assertEqual(
+                self.client.post(path, path_info.sample_post_data,
+                                 secure=True).status_code, 403,
+                'XSRF page accepts data missing XSRF token: %s' % path_name)
+            post_data = copy.deepcopy(path_info.sample_post_data)
+            post_data['xsrf_token'] = ''
+            self.assertEqual(
+                self.client.post(path, post_data, secure=True).status_code, 403,
+                'XSRF page accepts data empty XSRF token: %s' % path_name)
+
+    def test_invalid_xsrf_token(self):
+        """Tests that XSRF tokens are checked for validity."""
+        self.login(is_admin=True)
+        for (path_name, path_info) in self.get_paths_to_test(
+                lambda path_info: path_info.requires_xsrf):
+            path = django.urls.reverse(path_name)
+            post_data = copy.deepcopy(path_info.sample_post_data)
+            post_data['xsrf_token'] = 'NotAValidToken'
+            self.assertEqual(
+                self.client.post(path, post_data, secure=True).status_code, 403,
+                'XSRF page accepts data invalid XSRF token: %s' % path_name)
 
     def test_all_paths_included(self):
         """Tests that all (Django-served) pages are listed.
@@ -156,4 +205,4 @@ class AutoSecurityTests(view_tests_base.ViewTestsBase):
                 # Skip these; they're the same views as the non-prefixed
                 # versions.
                 continue
-            assert (pattern.name in AutoSecurityTests.PATH_TEST_INFO)
+            assert pattern.name in AutoSecurityTests.PATH_TEST_INFO
