9f82c14e73c8ad6bd95420571fe416c096f8012b
Nick Worden <9059304+nworden@users.noreply.github.com>
2019-04-14 14:12:37 -0400

    CSP settings.

--- app/settings.py
+++ app/settings.py
@@ -71,6 +71,14 @@ if site_settings.OPTIONAL_PATH_PREFIX:
   SECURE_REDIRECT_EXEMPT += [
       r'^%s/.*/tasks/.*' % site_settings.OPTIONAL_PATH_PREFIX]
 
+# Based on the Strict CSP example here:
+# https://csp.withgoogle.com/docs/strict-csp.html
+CSP_INCLUDE_NONCE_IN = ('script-src',)
+CSP_BASE_URI = "'none'"
+CSP_OBJECT_SRC = "'none'"
+CSP_SCRIPT_SRC = ("'unsafe-inline'", "'unsafe-eval'",
+                  "'strict-dynamic' https: http:",)
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
