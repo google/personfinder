9670e4b49d7207fee1dcc275005ff6cf9447f06e
Nick Worden <9059304+nworden@users.noreply.github.com>
2019-04-23 12:12:41 -0400

    Start of the admin ACLs page.

---
+++ app/views/admin/acls.py
@@ -0,0 +1,102 @@
+# Copyright 2019 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""The pages for granting/revoking admin access."""
+
+import datetime
+
+import django.shortcuts
+
+import modelmodule.admin_acls as admin_acls_model
+import utils
+import views.admin.base
+
+
+class AdminAclsView(views.admin.base.AdminBaseView):
+    """The admin ACLs view."""
+
+    ACTION_ID = 'admin/acls'
+
+    _EXPIRATION_DATE_FORMAT = '%Y-%m-%d'
+
+    def setup(self, request, *args, **kwargs):
+        super(AdminAclsView, self).setup(request, *args, **kwargs)
+
+    def get_params(self):
+        return views.base.read_params(
+            super(AdminAclsView, self).get_params(),
+            self.request,
+            post_params={
+                'edit_button': utils.strip,
+                'email_address': utils.strip,
+                'expiration_date': utils.strip,
+                'level': utils.strip,
+                'revoke_button': utils.strip,
+            })
+
+    def _render_form(self):
+        existing_acls = admin_acls_model.AdminAcl.all().filter(
+            'repo =', self.env.repo)
+        default_expiration_date = (
+            utils.get_utcnow() + datetime.timedelta(days=365))
+        return self.render(
+            'admin_acls.html',
+            existing_acls=existing_acls,
+            default_expiration_date=default_expiration_date,
+            xsrf_token=self.xsrf_tool.generate_token(self.env.user.user_id(),
+                                                     self.ACTION_ID))
+
+    def get(self, request, *args, **kwargs):
+        """Serves GET requests with a form and list of existing ACLs.
+
+        The returned page includes a list of all the existing ACls for the repo
+        (with an option to edit or revoke them) and a form to add a new admin.
+        """
+        del request, args, kwargs  # unused
+        return self._render_form()
+
+    def post(self, request, *args, **kwargs):
+        """Serves POST requests, making the requested changes.
+
+        This handler will edit, revoke, or add ACLs as requested, and then
+        return the same page used for GET requests.
+        """
+        del request, args, kwargs  # unused
+        self.enforce_xsrf(self.ACTION_ID)
+        email_address = self.params.email_address
+        if self.params.level == 'full':
+            level = admin_acls_model.AdminAcl.AccessLevel.FULL_ADMIN
+        elif self.params.level == 'moderator':
+            level = admin_acls_model.AdminAcl.AccessLevel.MODERATOR
+        expiration_date = datetime.datetime.strptime(
+            self.params.expiration_date, AdminAclsView._EXPIRATION_DATE_FORMAT)
+        if (self.params.get('edit_button', '') or
+            self.params.get('revoke_button', '')):
+            acls = admin_acls_model.AdminAcl.all().filter(
+                'repo =', self.env.repo).filter(
+                    'email_address =', email_address)
+            assert acls.count() == 1
+            acl = acls[0]
+            if self.params.get('edit_button', ''):
+                acl.access_level = level
+                acl.expiration_date = expiration_date
+                acl.put()
+            else:
+                acl.delete()
+        else:
+            admin_acls_model.AdminAcl(
+                repo=self.env.repo,
+                access_level=level,
+                email_address=email_address,
+                expiration_date=expiration_date).put()
+        return self._render_form()
