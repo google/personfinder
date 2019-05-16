a807dc471b484dde5ed1199b3708ef8ccc156a4f
Nick Worden <9059304+nworden@users.noreply.github.com>
2019-05-16 17:07:46 -0400

    Timezone fix.

--- ui/src/pages/Results.js
+++ ui/src/pages/Results.js
@@ -190,7 +190,7 @@ class SearchResultImpl extends Component {
       nameStr += ' (' + this.props.result.alternateNames.join(', ') + ')';
     }
     const formattedTimestamp = <FormattedDate
-        value={new Date(this.props.result.timestamp)}
+        value={new Date(this.props.result.timestamp + 'Z')}
         day='numeric'
         month='short'
         hour='numeric'
