676d62ed655a1bf8b7c38e1afb0c1aea4d2d7e59
ryosuketc <43229670+ryosuketc@users.noreply.github.com>
2021-01-21 23:57:00 +0000

    Update docker executable to use tools/gae_run intead of tools/gae

--- docker/gae-run-app.sh
+++ docker/gae-run-app.sh
@@ -8,5 +8,5 @@ IP_ADDR=`ip addr list eth0 | grep 'inet ' | cut -d' ' -f6 | cut -d'/' -f1`
 
 cd ${PERSONFINDER_DIR}
 echo "Starting Person Finder server"
-tools/gae run app --host ${IP_ADDR} --port ${PORT} --admin_host=${IP_ADDR} --admin_port=${HOST_PORT} --api_host=${IP_ADDR} --api_port=${API_PORT} --skip_sdk_update_check
+tools/gae_run app --host ${IP_ADDR} --port ${PORT} --admin_host=${IP_ADDR} --admin_port=${HOST_PORT} --api_host=${IP_ADDR} --api_port=${API_PORT} --skip_sdk_update_check
 echo "Person Finder server stopped"
